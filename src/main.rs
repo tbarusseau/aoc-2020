0fcb768bc840aa5ccaea3b1886cfc1e555638a49
Thomas BARUSSEAU <thomas.barusseau@thetribe.io>
2020-12-10 07:52:59 +0100

    Bye-bye cargo-aoc

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,11 @@
 #![allow(unused_imports)]
 
 mod days;
-use days::*;
 
-macro_rules! aoc_import {
-    ($n:literal) => {
-        seq_macro::seq! {N in 1..=$n { paste::paste!{
-            use day#N::{solve_part1 as [<day#N _solve_part1>], solve_part2 as [<day#N _solve_part2>]};
-        }}}
-    };
-}
+#[macro_use]
+mod utils;
+
+use days::*;
 
 use chrono::{Datelike, Local};
 use structopt::StructOpt;
@@ -37,35 +33,5 @@ fn main() {
         opt.days = Some(vec![today]);
     }
 
-    // macro_rules! aoc_match {
-    //     ($n:literal) => {
-    //         opt.days.unwrap().iter().for_each(|d| match d {
-    //             seq_macro::seq! {N in 1..=$n { paste::paste!{
-    //                 #N => {
-    //                     [<day#N _solve_part1()>;]
-    //                     [<day#N _solve_part2()>;]
-    //                 },
-    //             }}}
-    //             i @ _ => panic!("Unavailable day: {}", i),
-    //         });
-    //     };
-    // }
-
-    // aoc_match!(10)
-
-    opt.days.unwrap().iter().for_each(|d| match d {
-        1 => {
-            let input = std::fs::read_to_string(&format!("./input/2020/day{}.txt", 1))
-                .expect("Input file not found");
-            println!("Day 1, part 1: {:?}", day1_solve_part1(&input));
-            println!("Day 1, part 2: {:?}", day1_solve_part2(&input));
-        }
-        10 => {
-            let input = std::fs::read_to_string(&format!("./input/2020/day{}.txt", 10))
-                .expect("Input file not found");
-            println!("Day 10, part 1: {:?}", day10_solve_part1(&input));
-            println!("Day 10, part 2: {:?}", day10_solve_part2(&input));
-        }
-        i @ _ => panic!("Unavailable day: {}", i),
-    });
+    aoc_match!(opt, 10, 2020);
 }
---
+++ src/utils/macros.rs
@@ -0,0 +1,57 @@
+/// Import all solutions from day `1` to `n`
+macro_rules! aoc_import {
+    ($day:literal) => {
+        seq_macro::seq! {Day in 1..=$day { paste::paste!{
+            use day#Day::{solve_part1 as [<day#Day _solve_part1>], solve_part2 as [<day#Day _solve_part2>]};
+        }}}
+    };
+}
+
+/// Generate a `match` statement that that checks `opt.days` from `1` to `n`
+macro_rules! aoc_match {
+    ($opt:expr,$day:literal,$year:literal) => {
+        seq_macro::seq! {Day in 1..=$day { paste::paste!{
+        $opt.days
+            .unwrap_or_else(|| panic!("'opt.days' is None"))
+            .iter()
+            .for_each(|d| match d {
+                #(Day => {
+                    let path = format!("./input/{}/day{}.txt", $year, Day);
+                    let input = match std::fs::read_to_string(&path) {
+                        Ok(s) => s,
+                        Err(_) => {
+                            // Download the input file from adventofcode.com
+                            use std::io::{stdout, Write};
+                            use std::fs::OpenOptions;
+
+                            use curl::easy::{Easy, List};
+
+                            print!("Downloading input for day {}... ", Day);
+                            let mut easy = Easy::new();
+                            easy.url(&format!("https://adventofcode.com/{}/day/{}/input", $year, Day)).unwrap();
+                            easy.write_function(|data| {
+                                let mut file = OpenOptions::new()
+                                    .write(true)
+                                    .create(true)
+                                    .append(true)
+                                    .open(&format!("./input/{}/day{}.txt", $year, Day))
+                                    .expect("Couldn't open output file");
+                                file.write_all(data).expect("Couldn't write to file");
+                                Ok(data.len())
+                            }).unwrap();
+                            println!("Done!");
+                            easy.cookie("session=53616c7465645f5f24669579e985604dd998b25e1573ff317d30d1dedb5f72351600515acf3e32647867bfd35607acb1");
+                            let r = easy.perform().unwrap();
+
+                            std::fs::read_to_string(&path).expect("Couldn't read downloaded input")
+                        }
+                    };
+
+                    println!("Day {}, part 1: {:?}", Day, [<day#Day _solve_part1>](&input));
+                    println!("Day {}, part 2: {:?}", Day, [<day#Day _solve_part2>](&input));
+                },)*
+                i => panic!("Unavailable day: {}", i),
+            });
+        }}}
+    };
+}
