cf4b3d31692cb457a95c2879cbd0d1d5b1490e5f
Thomas BARUSSEAU <thomas.barusseau@thetribe.io>
2020-12-16 15:13:44 +0100

    Dirty day16

---
+++ src/days/y2020/day16.rs
@@ -0,0 +1,241 @@
+use regex::Regex;
+use std::collections::HashMap;
+
+#[derive(Debug)]
+struct Pair {
+    min: usize,
+    max: usize,
+}
+
+impl Pair {
+    fn is_inside(&self, value: usize) -> bool {
+        self.min <= value && self.max >= value
+    }
+}
+
+#[derive(Debug)]
+struct Rule<'a> {
+    name: &'a str,
+    first: Pair,
+    second: Pair,
+}
+
+impl<'a> Rule<'a> {
+    fn value_is_valid(&self, value: usize) -> bool {
+        self.first.is_inside(value) || self.second.is_inside(value)
+    }
+}
+
+fn generate_input(input: &str) -> (Vec<Rule>, Vec<usize>, Vec<Vec<usize>>) {
+    let mut rules: Vec<Rule> = vec![];
+    let mut lines = input.lines();
+    let mut my_ticket: Vec<usize> = vec![];
+    let mut nearby_tickets: Vec<Vec<usize>> = vec![];
+
+    let pairs_re =
+        Regex::new(r"^(?P<name>.+): (?P<min1>\d+)-(?P<max1>\d+) or (?P<min2>\d+)-(?P<max2>\d+)")
+            .unwrap();
+    let mut step = 0;
+
+    while let Some(l) = lines.next() {
+        if l.is_empty() {
+            step += 1;
+            continue;
+        }
+
+        match step {
+            0 => {
+                // Match rules
+                let caps = pairs_re.captures(l).expect("Line doesn't match pairs_re");
+                rules.push(Rule {
+                    name: caps.name("name").expect("No name found").as_str(),
+                    first: Pair {
+                        min: caps
+                            .name("min1")
+                            .expect("No min1 found")
+                            .as_str()
+                            .parse()
+                            .expect("Invalid min1"),
+                        max: caps
+                            .name("max1")
+                            .expect("No max1 found")
+                            .as_str()
+                            .parse()
+                            .expect("Invalid max1"),
+                    },
+                    second: Pair {
+                        min: caps
+                            .name("min2")
+                            .expect("No min2 found")
+                            .as_str()
+                            .parse()
+                            .expect("Invalid min2"),
+                        max: caps
+                            .name("max2")
+                            .expect("No max2 found")
+                            .as_str()
+                            .parse()
+                            .expect("Invalid max2"),
+                    },
+                })
+            }
+            1 => {
+                // Match my ticket
+                if l == "your ticket:" {
+                    continue;
+                }
+
+                my_ticket = l.split(',').flat_map(str::parse).collect();
+            }
+            2 => {
+                // Match nearby tickets
+                if l == "nearby tickets:" {
+                    continue;
+                }
+
+                nearby_tickets.push(l.split(',').flat_map(str::parse).collect());
+            }
+            i => panic!("Invalid step: {}", i),
+        }
+    }
+
+    (rules, my_ticket, nearby_tickets)
+}
+
+fn get_ticket_invalid_value(rules: &Vec<Rule>, ticket: &Vec<usize>) -> Option<usize> {
+    for &value in ticket.iter() {
+        if !rules.iter().any(|r| r.value_is_valid(value)) {
+            return Some(value);
+        }
+    }
+
+    None
+}
+
+fn get_value_invalid_rules<'a>(rules: &'a Vec<Rule>, value: usize) -> Vec<&'a str> {
+    let mut r = vec![];
+
+    for rule in rules.iter() {
+        if !rule.value_is_valid(value) {
+            r.push(rule.name);
+        }
+    }
+
+    r
+}
+
+pub fn solve_part1(input: &str) -> usize {
+    let (rules, _, nearby_tickets) = generate_input(input);
+    nearby_tickets
+        .iter()
+        .flat_map(|t| get_ticket_invalid_value(&rules, t))
+        .sum()
+}
+
+pub fn solve_part2(input: &str) -> usize {
+    let (rules, my_ticket, nearby_tickets) = generate_input(input);
+
+    // Discard invalid tickets
+    let valid_tickets: Vec<Vec<usize>> = nearby_tickets
+        .into_iter()
+        .filter(|t| get_ticket_invalid_value(&rules, &t).is_none())
+        .collect();
+
+    // Start with every rule's name as true
+    let mut validated_rules: Vec<HashMap<&str, bool>> = vec![];
+    for _ in 0..valid_tickets.len() {
+        validated_rules.push(rules.iter().map(|r| (r.name, true)).collect());
+    }
+
+    let rules_names: Vec<&str> = rules.iter().map(|r| r.name).collect();
+    let mut possible_rules: Vec<Vec<&str>> = vec![rules_names.clone(); rules_names.iter().count()];
+
+    // For each nearby ticket, eliminate whichever rules are impossible
+    valid_tickets.iter().for_each(|t| {
+        t.iter().enumerate().for_each(|(i, v)| {
+            get_value_invalid_rules(&rules, *v).iter().for_each(|&r| {
+                possible_rules[i].retain(|&v| v != r);
+                validated_rules[i].entry(r).and_modify(|v| *v = false);
+            });
+        });
+    });
+
+    // Safety check to ensure that the program doesn't go into an infinite loop :)
+    if possible_rules.iter().any(|r| r.len() == 0) {
+        panic!("Big issue! One item in possible_rules has no candidate");
+    }
+
+    while possible_rules.iter().any(|l| l.len() > 1) {
+        let confirmed_rules: Vec<&str> = possible_rules
+            .clone()
+            .iter()
+            .filter(|l| l.len() == 1)
+            .map(|l| l[0])
+            .collect();
+
+        possible_rules
+            .iter_mut()
+            .filter(|l| l.len() != 1)
+            .for_each(|l| {
+                l.retain(|e| !confirmed_rules.contains(e));
+            });
+    }
+
+    let indexes: Vec<usize> = possible_rules
+        .iter()
+        .map(|v| v[0])
+        .enumerate()
+        .filter(|(_, v)| v.starts_with("departure"))
+        .map(|(i, _)| i)
+        .collect();
+
+    my_ticket
+        .iter()
+        .enumerate()
+        .filter(|(i, _)| indexes.contains(i))
+        .map(|(_, v)| v)
+        .product()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    pub fn solve1() {
+        assert_eq!(
+            solve_part1(
+                "class: 1-3 or 5-7
+row: 6-11 or 33-44
+seat: 13-40 or 45-50
+
+your ticket:
+7,1,14
+
+nearby tickets:
+7,3,47
+40,4,50
+55,2,20
+38,6,12"
+            ),
+            71
+        );
+    }
+
+    #[test]
+    pub fn solve2() {
+        solve_part2(
+            "class: 0-1 or 4-19
+row 0-5 or 8-19
+seat: 0-13 or 16-19
+
+your ticket:
+11,12,13
+
+nearby tickets:
+3,9,18
+15,1,5
+5,14,9",
+        );
+    }
+}
--- src/days/y2020/day4.rs
+++ src/days/y2020/day4.rs
@@ -122,6 +122,7 @@ impl PassportBuilder {
     }
 }
 
+#[derive(Debug)]
 pub struct Passport {
     pub byr: usize,          // Birth Year
     pub iyr: usize,          // Issue Year
@@ -155,6 +156,7 @@ impl Passport {
     pub fn from(input: &str) -> Result<Passport, Vec<PassportCreationError>> {
         let mut pb = PassportBuilder::new();
         input[..input.len() - 1].split(' ').for_each(|w| {
+            println!("Word: {:?}", w);
             let mut split = w.split(':');
             let key = split.next().expect("Couldn't get key");
             let value = split.next().expect("Couldn't get value");
@@ -209,7 +211,7 @@ mod tests {
     use super::*;
 
     #[test]
-    pub fn test() {
+    pub fn test1() {
         assert_eq!(
             solve_part1(
                 "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd
@@ -228,38 +230,22 @@ iyr:2011 ecl:brn hgt:59in"
             ),
             2
         );
+    }
 
+    #[test]
+    pub fn test_passport_parsing() {
         assert_eq!(
             solve_part2(
-                "eyr:1972 cid:100
-hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926
-
-iyr:2019
-hcl:#602927 eyr:1967 hgt:170cm
-ecl:grn pid:012533040 byr:1946
-
-hcl:dab227 iyr:2012
-ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277
-
-hgt:59cm ecl:zzz
-eyr:2038 hcl:74454a iyr:2023
-pid:3556412378 byr:2007
-
-pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980
-hcl:#623a2f
-
-eyr:2029 ecl:blu cid:129 byr:1989
-iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm
-
-hcl:#888785
-hgt:164cm byr:2001 iyr:2015 cid:88
-pid:545766238 ecl:hzl
-eyr:2022
-
-iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719
-"
+                //                 "eyr:1972 cid:100 hcl:#18171d ecl:amb hgt:170 pid:186cm iyr:2018 byr:1926\n
+                // iyr:2019 hcl:#602927 eyr:1967 hgt:170cm ecl:grn pid:012533040 byr:1946\n
+                // hcl:dab227 iyr:2012 ecl:brn hgt:182cm pid:021572410 eyr:2020 byr:1992 cid:277\n
+                // hgt:59cm ecl:zzz eyr:2038 hcl:74454a iyr:2023 pid:3556412378 byr:2007\n
+                "pid:087499704 hgt:74in ecl:grn iyr:2012 eyr:2030 byr:1980 hcl:#623a2f\n
+eyr:2029 ecl:blu cid:129 byr:1989 iyr:2014 pid:896056539 hcl:#a97842 hgt:165cm\n
+hcl:#888785 hgt:164cm byr:2001 iyr:2015 cid:88 pid:545766238 ecl:hzl eyr:2022\n
+iyr:2010 hgt:158cm hcl:#b6652a ecl:blu byr:1944 eyr:2021 pid:093154719"
             ),
             4
-        );
+        )
     }
 }
